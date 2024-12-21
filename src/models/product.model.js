dbdffba92a2ab135039099d21e941e4aebad8747
Hassan Lawi <mhassanlawi@gmail.com>
2024-12-21 21:20:25 +0800

    Updated fields of model 'Product' and 'Product Variant'

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -11,13 +11,13 @@ const productSchema = mongoose.Schema({
   type: {
     type: String,
     enum: ["simple", "variable"],
-    required: true,
     default: "simple",
+    required: true,
   },
   regularPrice: {type: Number, min: 0, default: 0, required: true},
-  salePrice: {type: Number, min: 0, default: 0},
+  salePrice: {type: Number, min: 0, default: 0, required: true},
   SKU: String,
-  stockManagement: { type: Boolean, default: false },
+  stockManagement: { type: Boolean, default: false, required: true},
   // if stockManagement is true
   quantity: { type: Number, min: 0, default: 0, required: true },
   // if stockManagement is false
@@ -32,7 +32,7 @@ const productSchema = mongoose.Schema({
   imagesURL: [String],
   backgroundImageURL: String,
   tags: [String],
-  slug: String,
+  slug: { type: String, unique: true, required: true},
   category: { type: mongoose.Schema.ObjectId, ref: "Category" },
   subcategory: { type: mongoose.Schema.ObjectId, ref: "SubCategory" },
 });
--- src/models/productVariant.model.js
+++ src/models/productVariant.model.js
@@ -3,19 +3,19 @@ const mongoose = require("mongoose");
 const productVariantSchema = mongoose.Schema({
   product: { type: mongoose.Schema.ObjectId, ref: "Product", required: true },
   attributes: { type: [{ name: String, values: String }], required: true },
-  regularPrice: { type: Number, min: 0, required: true },
-  salePrice: { type: Number, min: 0, default: 0},
-  SKU: { type: String, required: true },
-  stockManagement: { type: Boolean, default: false},
+  regularPrice: {type: Number, min: 0, default: 0, required: true},
+  salePrice: {type: Number, min: 0, default: 0, required: true},
+  SKU: String,
+  stockManagement: { type: Boolean, default: false, required: true},
   // if stockManagement is true
-  quantity: { type: Number, min: 0, default: 0},
+  quantity: { type: Number, min: 0, default: 0, required: true },
   // if stockManagement is false
   stockStatus: {
     type: String,
     enum: ["In stock", "Out of stock"],
-    default: "In stock"
+    default: "In stock",
+    required: true,
   },
-  imagesURL: [String],
 });
 
 const ProductVariant = mongoose.model("ProductVariant", productVariantSchema);
