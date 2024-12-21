8475d364f5d3f8f71e6731eddd203775bedbe483
Hassan Lawi <mhassanlawi@gmail.com>
2024-12-21 14:21:31 +0800

    Implemented MongoDB and Mongoose ODM

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 const path = require('path');
+const mongoose = require('mongoose');
 
 // config.env
 dotenv.config({path: path.join(__dirname, 'config.env')})
 
 // init express
 const app = express();
+
+// connect to MongoDB
+const db = process.env.MONGODB_LOCAL_CONNECTION;
+mongoose.connect(db).then(() => {
+    console.log(`Connected to ${db}`);
+})
 
 // morgan middleware
 app.use(morgan('dev'));
