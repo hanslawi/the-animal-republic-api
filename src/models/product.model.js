6d014912fe80281bf97ac6d3321720a1e1744ce3
Hassan Lawi <mhassanlawi@gmail.com>
2024-12-21 19:09:38 +0800

    implemented slugify in model 'Product'

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require('slugify');
 
 const productSchema = mongoose.Schema({
   name: {
@@ -10,8 +11,21 @@ const productSchema = mongoose.Schema({
   imagesURL: [String],
   backgroundImageURL: String,
   tags: [String],
+  slug: String,
   category: { type: mongoose.Schema.ObjectId, ref: "Category" },
   subcategory: { type: mongoose.Schema.ObjectId, ref: "SubCategory" },
+});
+
+// slugify name eg. Premium Hoodies = premium-hoodies
+productSchema.pre("save", function (next) {
+  if (this.name) this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+productSchema.pre("findOneAndUpdate", function (next) {
+  const { name } = this.getUpdate();
+  if (name) this.set({ slug: slugify(name, { lower: true }) });
+  next();
 });
 
 const Product = mongoose.model("Product", productSchema);
