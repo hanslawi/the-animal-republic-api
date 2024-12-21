5e8e71f0a00317d6d90ee90d28d0b1714e425132
Hassan Lawi <mhassanlawi@gmail.com>
2024-12-21 17:00:05 +0800

    implemented slugify for subcategory

--- src/models/subcategory.model.js
+++ src/models/subcategory.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const subcategorySchema = new mongoose.Schema({
   name: {
@@ -9,6 +10,19 @@ const subcategorySchema = new mongoose.Schema({
   description: String,
   slug: String,
   category: { type: mongoose.Schema.ObjectId, ref: "Category" },
+});
+
+// slugify name eg. Premium Hoodies = premium-hoodies
+subcategorySchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  console.log(this.slug);
+  next();
+});
+
+subcategorySchema.pre("findOneAndUpdate", function (next) {
+  const { name } = this.getUpdate();
+  this.set({ slug: slugify(name, { lower: true }) });
+  next();
 });
 
 const SubCategory = mongoose.model("SubCategory", subcategorySchema);
