cfb5430f9bf78395e7ed7d2757699de707fb5061
Hassan Lawi <mhassanlawi@gmail.com>
2024-12-21 14:15:15 +0800

    implemented dotenv

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const dotenv = require('dotenv');
+const path = require('path');
 
+// config.env
+dotenv.config({path: path.join(__dirname, 'config.env')})
+
+// init express
 const app = express();
 
+// morgan middleware
 app.use(morgan('dev'));
 
 const port = process.env.PORT || 8080;
