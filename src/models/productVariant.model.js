acfdc4fe81d6d5c5526cbe2bc0cc27ef5ba49d10
Hassan Lawi <mhassanlawi@gmail.com>
2024-12-21 21:23:13 +0800

    Added field 'imagesURL' to model 'Product Variant'

--- src/models/productVariant.model.js
+++ src/models/productVariant.model.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const productVariantSchema = mongoose.Schema({
   product: { type: mongoose.Schema.ObjectId, ref: "Product", required: true },
   attributes: { type: [{ name: String, values: String }], required: true },
-  regularPrice: {type: Number, min: 0, default: 0, required: true},
-  salePrice: {type: Number, min: 0, default: 0, required: true},
+  regularPrice: { type: Number, min: 0, default: 0, required: true },
+  salePrice: { type: Number, min: 0, default: 0, required: true },
   SKU: String,
-  stockManagement: { type: Boolean, default: false, required: true},
+  stockManagement: { type: Boolean, default: false, required: true },
   // if stockManagement is true
   quantity: { type: Number, min: 0, default: 0, required: true },
   // if stockManagement is false
@@ -16,6 +16,7 @@ const productVariantSchema = mongoose.Schema({
     default: "In stock",
     required: true,
   },
+  imagesURL: [String],
 });
 
 const ProductVariant = mongoose.model("ProductVariant", productVariantSchema);
